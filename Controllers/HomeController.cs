d7432a6fae891e3154e58c652672d6fe6e513d8a
Isaac <isaacherutich@gmail.com>
2020-03-11 21:51:33 +0300

    firstcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AIoTuesdaysReview.Models;
+
+namespace AIoTuesdaysReview.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult IoT()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
